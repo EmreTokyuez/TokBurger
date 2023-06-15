39c2d153bf5a8774f6903ea273d607ca24eef7d2
git@tokyuez.net <github@tokyuez.net>
2023-06-15 14:48:57 +0200

    Initial Commit

---
+++ TokBurger/TokBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TokBurgerApp.swift
+//  TokBurger
+//
+//  Created by Emre Tokyüz on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TokBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
